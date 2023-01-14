d5b75d33da3eb21c39028ffa11062e5d4112a059
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-14 11:55:45 +0100

    change import order due to bootstrap overwriting index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
